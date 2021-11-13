4109e4e83a2103d201c24b4b9e6cee39ab27fc34
manju-summoner <manju_summoner@manjubox.net>
2021-11-13 23:04:05 +0900

    貼り付け側のプロジェクトを追加

---
+++ PastePngFromClipboardWithAlpha/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PastePngFromClipboardWithAlpha
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PasteByGetImage(object sender, RoutedEventArgs e)
+        {
+            var source = Clipboard.GetImage();
+            image.Source = source;
+        }
+
+        private void PasteByGetData(object sender, RoutedEventArgs e)
+        {
+            var dataObject = Clipboard.GetDataObject();
+            if (dataObject?.GetFormats()?.Contains("PNG") is not true) return;
+
+            using var stream = (MemoryStream)dataObject.GetData("PNG");
+
+            var source = new BitmapImage();
+            source.BeginInit();
+            source.StreamSource = stream;
+            source.CacheOption = BitmapCacheOption.OnLoad;
+            source.EndInit();
+
+            image.Source = source;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # ツール置き場
 ## CopyPngToClipboardWithAlpha
 PNG画像の透過情報を維持したまま画像をクリップボードにコピーするためのツール。  
-画像受け取り側で特別な対応が必要。
+画像の貼り付け側で特別な対応が必要。
+- Paint.NET
+- FireAlpaka
+等で利用可能。
+
+## PastePngFromClipboardWithAlpha
+PNG画像の透過情報を維持したまま画像をクリップボードから読み込むため処理を実装したサンプルプログラム。
+画像のコピー側で特別な対応が必要。
 
 ## LaunchSnippingTool
 Windows11のSnippingToolで指定した画像を開くためのツール。
